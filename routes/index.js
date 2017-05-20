7287516749a7c6eae6e9484cd509c80f83829e41
mortvif <mortvif01@gmail.com>
2017-05-20 17:38:33 +0200

    Añadido juego aleatorio. OK en local

--- routes/index.js
+++ routes/index.js
@@ -25,9 +25,15 @@ router.post('/quizzes',                    quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
-
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
+// Nuevas rutas de /quizzes random
+router.get('/quizzes/randomplay',          quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)' , quizController.randomcheck);
+router.get('/quizzes/randomnone',          quizController.randomnone)
+
+
 
 // Pagina de ayuda
 router.get('/help', function(req,res, next){
